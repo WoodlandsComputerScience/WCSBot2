28d5e17a8d1d7834b9142a7a9d148fe7f9da66d0
jcoptre <45333584+jcoptre@users.noreply.github.com>
2021-01-31 20:08:48 -0500

    Update roles.js

--- commands/roles.js
+++ commands/roles.js
@@ -1,1 +1,19 @@
-// make soon
+const Command = require('./command')
+const Roles = require('../utils/roles')
+const config = require('../config.json') // the ID of the roles are located in `config.roles.<a,b>`
+
+class JoinCommand extends Command {
+    getAliases() {
+        return ['roles'];
+    }
+  
+    async exec(msg, args) {
+        // todo
+    }
+
+    getHelp() {
+        return 'Lists out the available roles in the server';
+    }
+}
+
+module.exports = JoinCommand
