fbc9b4c7b5dc312b36f2f7a43acffa41fa5e54a7
jcoptre <45333584+jcoptre@users.noreply.github.com>
2021-02-01 14:39:49 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,21 @@
-// make soon
+const Command = require('./command')
+const Roles = require('../utils/roles')
+const config = require('../config.json') // the ID of the roles are located in `config.roles.<a,b>`
+
+class JoinCommand extends Command {
+    getAliases() {
+        return ['ping'];
+    }
+  
+    async exec(msg, args) {
+        if (msg.content === '+ping') {  
+            msg.channel.send(`🏓Latency is ${Date.now() - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        }
+    }
+
+    getHelp() {
+        return '...pong!';
+    }
+}
+
+module.exports = JoinCommand
